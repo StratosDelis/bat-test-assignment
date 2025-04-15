960c7fafa7e1e715dc482a7b0cc21323b877e985
Stratos Delis <stratosdelis21@gmail.com>
2025-04-15 15:29:54 +0300

    Create test_signal_strength funciton

--- test_bat_functions.py
+++ test_bat_functions.py
@@ -2,13 +2,30 @@
 
 import pytest
 from bat_functions import calculate_bat_power
+from bat_functions import signal_strength
 
-@pytest.mark.parametrize("a, expected",[
+@pytest.mark.parametrize("p, expectedPower",[
     (0, 0),
     (1, 42),
     (2, 84),
     (3, 126)
 ])
 
-def test_calc_bat_power(a, expected):
-    assert calculate_bat_power(a) == expected
+def test_calc_bat_power(p, expectedPower):
+    """
+    Tests Batman's power for different levels (0, 1, 2, 3)
+    """
+    assert calculate_bat_power(p) == expectedPower
+
+@pytest.mark.parametrize("d, expectedSignal",[
+    (0, 100.0),
+    (5, 50.0),
+    (10, 0),
+    (12, 0)
+])
+
+def test_signal_strength(d, expectedSignal):
+    """
+    Tests the signal strength for different distances (0km, 5km, 10km, 12km)
+    """
+    assert signal_strength(d) == expectedSignal
